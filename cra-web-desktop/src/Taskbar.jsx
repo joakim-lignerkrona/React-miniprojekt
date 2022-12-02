f7d662e14abdb07fc68a6f067a77f1ec3bb2b233
snillet1992 <johan.p.sellstrom@gmail.com>
2022-12-02 11:07:16 +0100

    new taskbar

--- cra-web-desktop/src/Taskbar.jsx
+++ cra-web-desktop/src/Taskbar.jsx
@@ -6,9 +6,9 @@ import clock from "./assets/clock.png"
 export default function Taskbar({ children }) {
   return (
   <div className="taskbar d-flex w-100"> 
-        <img src={start}></img>
-        <div className="flex-grow-1"><div style={{width: "100%"}} ><img src={middle} style={{maxHeight: "100%", maxWidth: "100%", height: "auto"}}></img></div></div>
-        <img src={clock}></img>
+        <img src={start} style={{height: "100%"}}></img>
+        <img className="flex-grow-1" src={middle} ></img>
+        <img src={clock} style={{height: "100%"}}></img>
   </div>
   );
 }
