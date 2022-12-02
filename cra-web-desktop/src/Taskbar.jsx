dd86afc9d438f068135b0f01fb46d8a7f3902533
snillet1992 <johan.p.sellstrom@gmail.com>
2022-12-02 16:41:40 +0100

    new

--- cra-web-desktop/src/Taskbar.jsx
+++ cra-web-desktop/src/Taskbar.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import start from "./assets/start.png";
 import middle from "./assets/taskbar.middle.png";
 import clock from "./assets/clock.png";
+import startmenu from "./desktop/Start-menu";
 
 export default function Taskbar({ children }) {
   return (
     <div className="taskbar d-flex w-100">
-      <img src={start} style={{ height: "100%" }}></img>
+      <img src={start} onClick={startmenu} style={{ height: "100%" }}></img>
 
       <div
         className="flex-grow-1"
--- cra-web-desktop/src/index.js
+++ cra-web-desktop/src/index.js
@@ -35,7 +35,7 @@ root.render(
         <DesktopIcon src="https://winaero.com/blog/wp-content/uploads/2017/01/clock-time-icon.png" appName="Timer" appToStart={<Timer/>} />
         <DesktopIcon src="https://cdn2.iconfinder.com/data/icons/icons-mega-pack-1-and-2/256/Windows_Media_Player_10.png" appName="Music" appToStart={<Music videoId={"dQw4w9WgXcQ"}/>}/>
         <DesktopIcon src={paintLogo} appName="Paint" appToStart={<Paint />}/>
-
+        <Startmenu/>
       </Desktop>
       <App />
     </Background>
