9d488f908cfeaac1c80d3cdd3118486349b78ed3
Christian von Bothmer <cvb.jaf@gmail.com>
2022-12-02 14:11:58 +0100

    remove extra clock

--- cra-web-desktop/src/App.jsx
+++ cra-web-desktop/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import Paint from "./apps/paint";
+import Paint from "./apps/Paint";
 import Timer from "./apps/Timer";
 import Clock from "./apps/clock";
 
@@ -23,7 +23,7 @@ function App() {
 
       {/* <Paint></Paint> */}
 
-      <Clock />
+    
 
       {/* <Music></Music> */}
 
