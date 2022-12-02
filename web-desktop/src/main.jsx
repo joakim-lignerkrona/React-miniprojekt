f234c087def914261bdd2824f2983782d36f845c
Joakim Lignerkrona <jockan@icloud.com>
2022-12-02 09:00:36 +0100

    Skal

--- web-desktop/src/main.jsx
+++ web-desktop/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import Background from "./Background";
+import Desktop from "./desktop";
+import DesktopIcon from "./desktop/DesktopIcon";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Background>
+      <Desktop>
+        <DesktopIcon src="/vite.svg" appName="Timer" />
+        <DesktopIcon src="/vite.svg" appName="ToDo" />
+        <DesktopIcon src="/vite.svg" appName="Paint" />
+      </Desktop>
+      <App />
+    </Background>
   </React.StrictMode>
-)
+);
