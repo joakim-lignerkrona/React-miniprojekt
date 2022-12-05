b6646c11a7b191f4df9dcac46d8033f5e25bc81e
Joakim Lignerkrona <jockan@icloud.com>
2022-12-05 11:15:29 +0100

    delete vite version

--- web-desktop/src/Background.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Background({ children }) {
-  return <div className="background">{children}</div>;
-}
--- web-desktop/src/Desktop.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Desktop({ children }) {
-  return (
-    <>
-      <div className="desktop">{children}</div>
-    </>
-  );
-}
